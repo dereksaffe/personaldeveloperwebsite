665a4ef793157c8d2951f498d13d536407453384
Derek Saffe <desaff@gmail.com>
2020-10-14 12:39:44 -0300

    changed seo to derek saffe portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ import { splashScreen } from "../config"
 
 const IndexPage = ({ data }) => (
   <Layout splashScreen={splashScreen}>
-    <SEO title="Portfolio Minimal - A Gatsby Starter." />
+    <SEO title="Derek Saffe Portfolio" />
     <Hero content={data.hero.edges} />
     {/* Articles is populated via Medium RSS Feed fetch */}
     <About content={data.about.edges} />
